7aa9805e6fc6dd9dc75879681f910772a067ce85
Thomas Morcom <thomascmorcom@gmail.com>
2023-09-26 17:44:40 +1300

    Updated Speedo, new font is working, but it blinks

--- Speedometer_v3.ino
+++ Speedometer_v3.ino
@@ -12,11 +12,11 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
 );
 
 // Uncomment for ST7262 IPS LCD 800x480
- Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-   bus,
-   800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
-   480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
-   1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
+    bus,
+    800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
+    480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
 /*******************************************************************************
  * End of Arduino_GFX setting
  ******************************************************************************/
@@ -31,13 +31,14 @@ const int I2Caddress = 8;
 static int16_t screenWidth, screenHeight, center;
 
 // Measured in Pixels
-const int charWidth = 36;
-const int charHeight = 74;
+const int charWidth = 33;
+const int charHeight = 71;
 const int charSpace = 5;
 // 25 for defualt font
 const int speedCharScale = 3;
 
-struct rectGraph {
+struct rectGraph
+{
   char *str;
   int x;
   int y;
@@ -54,12 +55,13 @@ struct rectGraph speedCoordinates; // Not exactly a graph but the struct is good
 
 int prevSpeed = 0;
 
-double drawVerticalGraph (struct rectGraph rect, int percentValue) {
-  
+double drawVerticalGraph(struct rectGraph rect, int percentValue)
+{
+
   rect.x++;
   rect.y++;
   rect.width -= 2;
-  rect.height -=2;
+  rect.height -= 2;
 
   // pv = percentValue, which is how much of the graph is to be coloured in
   // e.g., 100 is the full graph, 25 is the bottom quarter
@@ -68,33 +70,36 @@ double drawVerticalGraph (struct rectGraph rect, int percentValue) {
   int pvDiff;
 
   // Calculate the difference between previouis graph shown, and what will be shown this time
-  pvDiff = rect.height * ((rect.oldValue - percentValue)/100.00) +1;
-  if (pvDiff < 0) pvDiff = 0;
+  pvDiff = rect.height * ((rect.oldValue - percentValue) / 100.00) + 1;
+  if (pvDiff < 0)
+    pvDiff = 0;
   // Calculate how the graph tall that will shown is
-  pvHeight = rect.height * (percentValue/100.00);
+  pvHeight = rect.height * (percentValue / 100.00);
   // Find the y value of the graph
   y = rect.height - pvHeight + rect.y;
   // Find the y value for the Blank rectangle that will be drawn over the old graph
   yPvDiff = y - pvDiff;
 
   // Blank old graph
-  gfx->fillRect(rect.x,yPvDiff,rect.width,pvDiff,BACKGROUND);
+  gfx->fillRect(rect.x, yPvDiff, rect.width, pvDiff, BACKGROUND);
 
   // Draw new graph, and only draw it if the colour is changing
   // if ((rect.oldValue <= percentValue) || (percentValue < 50 && rect.oldValue >= 50) || (percentValue < 25 && rect.oldValue >= 25)) {
-  if (rect.oldValue <= percentValue) {
-    gfx->fillRect(rect.x,y,rect.width,pvHeight,c);
-  } 
+  if (rect.oldValue <= percentValue)
+  {
+    gfx->fillRect(rect.x, y, rect.width, pvHeight, c);
+  }
 
   return percentValue;
 }
 
-double drawHorizontalGraph (struct rectGraph rect, int percentValue) {
-  
+double drawHorizontalGraph(struct rectGraph rect, int percentValue)
+{
+
   rect.x++;
   rect.y++;
   rect.width -= 2;
-  rect.height -=2;
+  rect.height -= 2;
 
   // pv = percentValue, which is how much of the graph is to be coloured in
   // e.g., 100 is the full graph, 25 is the bottom quarter
@@ -103,34 +108,42 @@ double drawHorizontalGraph (struct rectGraph rect, int percentValue) {
   int pvDiff;
 
   // Define the colour that the new graph will be.
-  if (percentValue > 50) {
+  if (percentValue > 50)
+  {
     c = GOOD_BATTERY;
-  } else if (percentValue > 25) {
+  }
+  else if (percentValue > 25)
+  {
     c = AVERAGE_BATTERY;
-  } else {
+  }
+  else
+  {
     c = BAD_BATTERY;
   }
 
   // Calculate the difference between previouis graph shown, and what will be shown this time
-  pvDiff = rect.width * ((rect.oldValue - percentValue)/100.00) +1;
-  if (pvDiff < 0) pvDiff = 0;
+  pvDiff = rect.width * ((rect.oldValue - percentValue) / 100.00) + 1;
+  if (pvDiff < 0)
+    pvDiff = 0;
   // Calculate how the graph wide that will shown is
-  pvWidth = rect.width * (percentValue/100.00);
+  pvWidth = rect.width * (percentValue / 100.00);
   // Find the y value for the Blank rectangle that will be drawn over the old graph
-  xPvDiff = pvWidth + rect.x ;
+  xPvDiff = pvWidth + rect.x;
 
   // Blank old graph
-  gfx->fillRect(xPvDiff,rect.y,pvDiff,rect.height,BACKGROUND);
+  gfx->fillRect(xPvDiff, rect.y, pvDiff, rect.height, BACKGROUND);
 
   // Draw new graph, and only draw it if the colour is changing
-  if ((rect.oldValue <= percentValue) || (percentValue < 50 && rect.oldValue >= 50) || (percentValue < 25 && rect.oldValue >= 25)) {
-    gfx->fillRect(rect.x,rect.y,pvWidth,rect.height,c);
-  } 
+  if ((rect.oldValue <= percentValue) || (percentValue < 50 && rect.oldValue >= 50) || (percentValue < 25 && rect.oldValue >= 25))
+  {
+    gfx->fillRect(rect.x, rect.y, pvWidth, rect.height, c);
+  }
 
   return percentValue;
 }
 
-void drawString (int x, int y, char *str) {
+void drawString(int x, int y, char *str)
+{
   int scale = 3;
 
   gfx->setCursor(x, y);
@@ -139,79 +152,83 @@ void drawString (int x, int y, char *str) {
   gfx->println(str);
 }
 
-void drawGraphSetup (struct rectGraph rect) {
-  gfx->drawRect(rect.x,rect.y,rect.width,rect.height,WHITE);
+void drawGraphSetup(struct rectGraph rect)
+{
+  gfx->drawRect(rect.x, rect.y, rect.width, rect.height, WHITE);
   drawString(rect.x, rect.y + rect.height + 10, rect.str);
 }
 
 // Each character is 5x7 pixels, with 1 pixel space between them
-void drawSpeedNumbers(int x, int y, int speed) {
+void drawSpeedNumbers(int x, int y, int speed)
+{
 
-  if (prevSpeed == speed) return;
+  if (prevSpeed == speed)
+    return;
   // printf("Drawing a new speed\n");
 
   int numOfChars = 0;
-  int h = charHeight*speedCharScale;
-  int w = charWidth*speedCharScale;
-  int s = charSpace*speedCharScale;
+  int h = charHeight * speedCharScale;
+  int w = charWidth * speedCharScale;
+  int s = charSpace * speedCharScale;
 
-  // gfx->fillRect(x,y-h,w,h,PURPLE);
-  // gfx->drawRect(x+w,y-h,60,h,YELLOW);
-  // gfx->fillRect(x+w+s,y-h,w,h,ORANGE);
-  // gfx->drawRect(x+w+s+w,y-h,60,h,YELLOW);
-  // gfx->fillRect(x+w+s+w+s,y-h,w,h,RED);
+  // gfx->fillRect(x+s,y-h,(3*w + 3*s),h+speedCharScale,YELLOW);
+  // gfx->fillRect(x+s+w+s+w+s,y-h,w+s,h+speedCharScale,PURPLE);
+  // gfx->fillRect(x,y,1,100,RED);
 
-  gfx->fillRect(x,y-h,(3*charWidth + 2*charSpace)*speedCharScale,h+speedCharScale,BACKGROUND);
-  // gfx->fillRect(x,y-h,w,h+1,BACKGROUND);
-  // gfx->fillRect(x+w+s,y-h,w,h+1,BACKGROUND);
-  // gfx->fillRect(x+w+s+w+s,y-h,w,h+1,BACKGROUND);
+  if ((prevSpeed > 99 && speed < 100) || (prevSpeed > 9 && speed < 10))
+  {
+    printf("Manually Blanking the speed display\n");
+    printf("BLANKING --> X: %d, Y: %d, Width: %d, Height: %d\n", x, y - h, (3 * w + 3 * s), h + speedCharScale);
 
-
-  if ((prevSpeed > 99 && speed < 100) || (prevSpeed > 9 && speed < 10)) {
-    // gfx->fillRect(x, y, (3*charWidth + 2*charSpace)*speedCharScale, charHeight*speedCharScale, BACKGROUND);
-    // printf("Manually Blanking the speed display\n");
-    // printf("BLANKING --> X: %d, Y: %d, Width: %d, Height: %d\n", x, y, blankWidth, blankHeight);
-
+    gfx->fillRect(x, y - h, (3 * w + 3 * s), h + 1, BACKGROUND);
     // gfx->fillRect(x, y-h+51, (3*charWidth)*speedCharScale, charHeight*speedCharScale, BACKGROUND);
-    gfx->fillRect(x, y, (3*charWidth + 2*charSpace)*speedCharScale, charHeight*speedCharScale+1, BACKGROUND);
-  } 
+  }
 
-  if (speed < 10) {
+  if (speed < 10)
+  {
     numOfChars = 1;
-    x += 2*charWidth*speedCharScale;
-  } else if (speed < 100) {
+    x += 2 * (w + s) + s;
+    gfx->fillRect(x, y - h, w + s, h + speedCharScale, BACKGROUND);
+  }
+  else if (speed < 100)
+  {
     numOfChars = 2;
-    x += charWidth*speedCharScale;
+    x += w + 2 * s;
+    gfx->fillRect(x, y - h, 2 * (w + s), h + speedCharScale, BACKGROUND);
   }
-  else {
+  else
+  {
     numOfChars = 3;
+    gfx->fillRect(x, y - h, 3 * (w + s) + s, h + speedCharScale, BACKGROUND);
   }
+
+  // gfx->fillRect(x,y-h,(3*w+3*s)+s,h+speedCharScale,RED);
 
   gfx->setCursor(x, y);
   // gfx->setFont(u8g2_font_inb63_mn);
   gfx->setFont(u8g2_font_logisoso62_tn);
-  // gfx->setFont(u8g2_font_logisoso78_tn);
   // First colour is the Text Color, the next is the background color
   gfx->setTextColor(WHITE, BACKGROUND);
   gfx->setTextSize(speedCharScale);
   gfx->println(speed);
 
-  // gfx->fillRect(x,y,(3*charWidth + 2*charSpace)*speedCharScale,speedCharScale,BACKGROUND);
-
   prevSpeed = speed;
 }
 
-void receiveNumber(int numBytes) {
+void receiveNumber(int numBytes)
+{
   Serial.println("In receiveNumber method");
   int receivedData;
-  while (Wire.available()) {
-    Wire.readBytes((uint8_t*)&receivedData, sizeof(receivedData)); // Read the integer from the I2C bus
+  while (Wire.available())
+  {
+    Wire.readBytes((uint8_t *)&receivedData, sizeof(receivedData)); // Read the integer from the I2C bus
     Serial.print("Received integer: ");
     Serial.println(receivedData);
   }
 }
 
-void setup() {
+void setup()
+{
   Wire.begin(I2Caddress);
   Wire.onReceive(receiveNumber);
 
@@ -226,35 +243,34 @@ void setup() {
 
   // Set parameteres for Rectangular Bar Graphs
   // {Str, x, y, width, height, oldValue}
-  batterySocGraph = {"BATTERY SOC", 0,0,screenWidth,100, 0.0};
-  powerGraph = {"PWR", screenWidth - 100,200,50,200, 0.0};
-  brakeGraph = {"BRK", screenWidth - 200,200,50,200, 0.0};
-  acceleratorGraph = {"ACL", screenWidth - 300,200,50,200, 0.0};
-  // speedCoordinates = {"KM/H",25,200,3*charWidth*speedCharScale,charHeight*speedCharScale, 0.0};
-  speedCoordinates = {"KM/H",10,375,3*charWidth*speedCharScale,charHeight*speedCharScale, 0.0};
+  batterySocGraph = {"BATTERY SOC", 0, 0, screenWidth, 100, 0.0};
+  powerGraph = {"PWR", screenWidth - 100, 200, 50, 200, 0.0};
+  brakeGraph = {"BRK", screenWidth - 200, 200, 50, 200, 0.0};
+  acceleratorGraph = {"ACL", screenWidth - 300, 200, 50, 200, 0.0};
+  speedCoordinates = {"KM/H", 10, 375, 3 * charWidth * speedCharScale, charHeight * speedCharScale, 0.0};
 
   // Draw and setup graph outlines and texts
   drawGraphSetup(batterySocGraph);
   drawGraphSetup(powerGraph);
   drawGraphSetup(brakeGraph);
   drawGraphSetup(acceleratorGraph);
-  // drawString(speedCoordinates.x+speedCoordinates.width-speedCharScale, speedCoordinates.y+speedCoordinates.height+10+speedCharScale, speedCoordinates.str);
-  drawString(speedCoordinates.x+360, speedCoordinates.y-21, speedCoordinates.str);
+  drawString(speedCoordinates.x + 360, speedCoordinates.y - 21, speedCoordinates.str);
   // drawString(speedCoordinates.x+390, speedCoordinates.y+50, speedCoordinates.str);
-  // drawString(speedCoordinates.x+speedCoordinates.width-speedCharScale, speedCoordinates.y+speedCoordinates.height+10+175+speedCharScale, speedCoordinates.str);
 
   printf("---END OF SETUP---\n");
 }
 
-void loop() {
-  for (int i = 0; i < 100; i++){
-  // for (int i = 99; i > -1; i--){
-    batterySocGraph.oldValue = drawHorizontalGraph(batterySocGraph, 100-i);
-    powerGraph.oldValue = drawVerticalGraph(powerGraph, ((100-i)%50)*2);
-    brakeGraph.oldValue = drawVerticalGraph(brakeGraph, ((100-i)%20)*5);
-    acceleratorGraph.oldValue = drawVerticalGraph(acceleratorGraph, ((i)%20)*5);
-    drawSpeedNumbers(speedCoordinates.x, speedCoordinates.y, i+50);
+void loop()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    // for (int i = 99; i > -1; i--){
+    batterySocGraph.oldValue = drawHorizontalGraph(batterySocGraph, 100 - i);
+    powerGraph.oldValue = drawVerticalGraph(powerGraph, ((100 - i) % 50) * 2);
+    brakeGraph.oldValue = drawVerticalGraph(brakeGraph, ((100 - i) % 20) * 5);
+    acceleratorGraph.oldValue = drawVerticalGraph(acceleratorGraph, ((i) % 20) * 5);
+    drawSpeedNumbers(speedCoordinates.x, speedCoordinates.y, i * 2);
 
-    delay(2);
+    delay(100);
   }
 }
